11e0a70ad384a94307931c8a250ecadf0a82e821
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-10-29 18:44:59 +0100

    add Constants class for game configuration

---
+++ core/src/main/java/com/java_game_project/utils/Constants.java
@@ -0,0 +1,19 @@
+package com.java_game_project.utils;
+
+public abstract class Constants {
+    public static final String GAME_TITLE = "KaelSurvival";
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 500;
+
+    public static final String SKIN = "ui/glassy-ui.json";
+    public static final String FONT = "ui/font-export.fnt";
+    public static final String BACKGROUND = "images/background.jpg";
+
+
+    public static final String MUSICS_PATH = "assets/musics";
+    public static final String MENU_MUSIC = "menu_music";
+    public static final String INTRO_MUSIC = "intro_music";
+
+    public static final String SOUNDS_PATH = "assets/sounds";
+    public static final String MOUSE_CLICK_SOUND = "mouse_click";
+}
