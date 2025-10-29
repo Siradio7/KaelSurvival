d4fce6dcc2caf67dc1355dcd5f00b28802294bbd
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-10-29 17:37:26 +0100

    First commit

---
+++ lwjgl3/src/main/java/com/java_game_project/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,31 @@
+package com.java_game_project.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.java_game_project.Main;
+
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        if (StartupHelper.startNewJvmIfRequired()) {
+            return;
+        }
+
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+
+        configuration.setTitle("KaelSurvival");
+        configuration.useVsync(true);
+        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        configuration.setWindowedMode(840, 480);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+
+        return configuration;
+    }
+}
