171b9357b9ba7f54067e94a656e9a972fb2f3682
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-10-29 18:46:17 +0100

    refactor Lwjgl3Launcher to use Constants for game title and window dimensions

--- lwjgl3/src/main/java/com/java_game_project/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/java_game_project/lwjgl3/Lwjgl3Launcher.java
@@ -3,6 +3,7 @@ package com.java_game_project.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.java_game_project.Main;
+import com.java_game_project.utils.Constants;
 
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
@@ -20,10 +21,10 @@ public class Lwjgl3Launcher {
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
 
-        configuration.setTitle("KaelSurvival");
+        configuration.setTitle(Constants.GAME_TITLE);
         configuration.useVsync(true);
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
-        configuration.setWindowedMode(840, 480);
+        configuration.setWindowedMode(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
 
         return configuration;
