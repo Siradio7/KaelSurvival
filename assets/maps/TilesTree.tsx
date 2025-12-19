a48c1f5848a118933133c19055cc255a66301cad
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-12-19 14:58:26 +0100

    add level 3 map and associated tilesets for enhanced gameplay

---
+++ assets/maps/TilesTree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TilesTree" tilewidth="50" tileheight="30" tilecount="24" columns="3">
+ <image source="TilesTree.png" width="150" height="250"/>
+</tileset>
