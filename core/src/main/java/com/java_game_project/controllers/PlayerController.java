f5828f74ff76391a4d29144d7361b40c5162d483
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-12-23 03:38:04 +0100

    add PlayerController class for handling player input and movement

---
+++ core/src/main/java/com/java_game_project/controllers/PlayerController.java
@@ -0,0 +1,32 @@
+package com.java_game_project.controllers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.java_game_project.models.Player;
+
+public class PlayerController {
+    private final Player player;
+
+    public PlayerController(Player player) {
+        this.player = player;
+    }
+
+    public Player update() {
+        float vx = 0;
+        float vy = 0;
+
+        if (Gdx.input.isKeyPressed(Keys.Z) || Gdx.input.isKeyPressed(Keys.UP)) vy = player.getSpeed();
+        if (Gdx.input.isKeyPressed(Keys.S) || Gdx.input.isKeyPressed(Keys.DOWN)) vy = -player.getSpeed();
+        if (Gdx.input.isKeyPressed(Keys.Q) || Gdx.input.isKeyPressed(Keys.LEFT)) vx = -player.getSpeed();
+        if (Gdx.input.isKeyPressed(Keys.D) || Gdx.input.isKeyPressed(Keys.RIGHT)) vx = player.getSpeed();
+
+        player.getVelocity().set(vx, vy);
+
+        if (vx != 0 || vy != 0) {
+            float angle = (float) Math.toDegrees(Math.atan2(vx, -vy));
+            player.setRotation(angle);
+        }
+
+        return player;
+    }
+}
