fe1e1a3999db1cc2790216feabcdf7dc96e16516
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-12-23 03:36:41 +0100

    add Player class for player entity representation and movement logic

---
+++ core/src/main/java/com/java_game_project/models/Player.java
@@ -0,0 +1,41 @@
+package com.java_game_project.models;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+
+public class Player extends EntityModel {
+    public Player(float x, float y, float width, float height) {
+        super(x, y, width, height);
+        this.speed = 100f;
+    }
+
+    public void update(float delta, Array<Rectangle> obstacles) {
+        float oldX = position.x;
+        float oldY = position.y;
+
+        position.x += velocity.x * delta;
+        bounds.setPosition(position);
+
+        if (checkCollisions(obstacles)) {
+            position.x = oldX;
+        }
+
+        position.y += velocity.y * delta;
+        bounds.setPosition(position);
+
+        if (checkCollisions(obstacles)) {
+            position.y = oldY;
+        }
+
+        clampToMap();
+        bounds.setPosition(position);
+    }
+
+    private boolean checkCollisions(Array<Rectangle> obstacles) {
+        for  (Rectangle obstacle : obstacles) {
+            if (bounds.overlaps(obstacle)) return true;
+        }
+
+        return false;
+    }
+}
