4b5e1be0f2550da83dee5ab40ca0799af874fb58
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-12-24 03:41:10 +0100

    add AnimationUtils for loading animations from texture sheets

---
+++ core/src/main/java/com/java_game_project/utils/AnimationUtils.java
@@ -0,0 +1,25 @@
+package com.java_game_project.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class AnimationUtils {
+    public static Animation<TextureRegion> loadAnimation(String path, int frameWidth, int frameHeight, float frameDuration) {
+        Texture sheet = AssetLoader.getTexture(path);
+
+        TextureRegion[][] tmp = TextureRegion.split(sheet, frameWidth, frameHeight);
+        int cols = sheet.getWidth() / frameWidth;
+        int rows = sheet.getHeight() / frameHeight;
+        TextureRegion[] frames = new TextureRegion[cols * rows];
+
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                frames[index++] = tmp[i][j];
+            }
+        }
+
+        return new Animation<>(frameDuration, frames);
+    }
+}
