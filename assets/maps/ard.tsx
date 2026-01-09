55315d6f48ee359d1a30519f6975f118d009ef97
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2026-01-10 00:07:56 +0100

    add level 2 map and related textures

---
+++ assets/maps/ard.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Soil_Tile" tilewidth="50" tileheight="30" tilecount="170" columns="10">
+ <image source="Soil_Tile.png" width="512" height="512"/>
+</tileset>
