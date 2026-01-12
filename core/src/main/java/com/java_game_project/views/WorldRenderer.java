e4723af0b2103569e218776a0d2b9df33410b76d
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2026-01-12 23:51:53 +0100

    robust asset loading

--- core/src/main/java/com/java_game_project/views/WorldRenderer.java
+++ core/src/main/java/com/java_game_project/views/WorldRenderer.java
@@ -13,6 +13,7 @@ import com.java_game_project.models.*;
 import com.java_game_project.utils.AnimationUtils;
 import com.java_game_project.utils.EntityState;
 import com.java_game_project.utils.MapManager;
+import com.java_game_project.utils.AssetLoader;
 
 public class WorldRenderer {
     private final GameWorld world;
@@ -31,23 +32,23 @@ public class WorldRenderer {
     public WorldRenderer(GameWorld world) {
         this.world = world;
 
-        this.treeTex = new Texture(Gdx.files.internal("maps/TilesTree.png"));
-        this.caveTex = new Texture(Gdx.files.internal("maps/mystic_cave.png"));
-        this.exitTex = new Texture(Gdx.files.internal("images/portail.png"));
-        this.projectileTex = new Texture(Gdx.files.internal("images/arrow.png"));
+        this.treeTex = AssetLoader.getTexture("maps/TilesTree.png");
+        this.caveTex = AssetLoader.getTexture("maps/mystic_cave.png");
+        this.exitTex = AssetLoader.getTexture("images/portail.png");
+        this.projectileTex = AssetLoader.getTexture("images/arrow.png");
         this.arrowRegion = new TextureRegion(projectileTex);
 
         this.playerWalkAnim = AnimationUtils.loadAnimation("images/player_walk.png", 250, 250, 0.1f);
         this.playerAttackAnim = AnimationUtils.loadAnimation("images/kael_attack.png", 250, 250, 0.15f);
         this.orkWalkAnim = AnimationUtils.loadAnimation("images/ork_attack.png", 250, 250, 0.1f);
 
-        this.playerIdle = new TextureRegion(new Texture(Gdx.files.internal("images/player.png")));
-        this.orkIdle = new TextureRegion(new Texture(Gdx.files.internal("maps/ork.png")));
+        this.playerIdle = new TextureRegion(AssetLoader.getTexture("images/player.png"));
+        this.orkIdle = new TextureRegion(AssetLoader.getTexture("maps/ork.png"));
 
-        this.pouletWalkAnim = AnimationUtils.loadAnimation("images/chicken.png",32, 32,0.15f);
+        this.pouletWalkAnim = AnimationUtils.loadAnimation("images/chicken.png", 32, 32, 0.15f);
         this.pouletIdle = pouletWalkAnim.getKeyFrames()[0];
 
-        this.moutonWalkAnim = AnimationUtils.loadAnimation("images/sheep.png",32, 32,0.15f);
+        this.moutonWalkAnim = AnimationUtils.loadAnimation("images/sheep.png", 32, 32, 0.15f);
         this.moutonIdle = moutonWalkAnim.getKeyFrames()[0];
 
         this.font = new BitmapFont();
@@ -73,15 +74,15 @@ public class WorldRenderer {
                 }
 
                 batch.draw(currentFrame, p.getPosition().x, p.getPosition().y, p.getBounds().width / 2,
-                    p.getBounds().height / 2, p.getBounds().width, p.getBounds().height, 1, 1, p.getRotation());
+                        p.getBounds().height / 2, p.getBounds().width, p.getBounds().height, 1, 1, p.getRotation());
             }
         }
 
         for (com.java_game_project.models.Projectile proj : world.getProjectiles()) {
             Vector2 visualPos = proj.getVisualPosition();
             batch.draw(arrowRegion, visualPos.x, visualPos.y,
-                proj.getBounds().width / 2, proj.getBounds().height / 2,
-                proj.getBounds().width, proj.getBounds().height, 1, 1, proj.getVisualRotation());
+                    proj.getBounds().width / 2, proj.getBounds().height / 2,
+                    proj.getBounds().width, proj.getBounds().height, 1, 1, proj.getVisualRotation());
         }
 
         for (Rectangle r : world.getTrees()) {
@@ -90,42 +91,40 @@ public class WorldRenderer {
 
         for (Ork o : world.getOrks()) {
             TextureRegion currentFrame = (o.getState() == EntityState.WALKING)
-                ? orkWalkAnim.getKeyFrame(o.getStateTime(), true)
-                : orkIdle;
+                    ? orkWalkAnim.getKeyFrame(o.getStateTime(), true)
+                    : orkIdle;
 
             batch.draw(currentFrame, o.getPosition().x, o.getPosition().y, o.getBounds().width / 2,
-                o.getBounds().height / 2, o.getBounds().width, o.getBounds().height, 1, 1, o.getRotation());
+                    o.getBounds().height / 2, o.getBounds().width, o.getBounds().height, 1, 1, o.getRotation());
         }
 
         for (Poulet poulet : world.getPoulets()) {
-            TextureRegion frame =
-                (poulet.getState() == EntityState.WALKING)
+            TextureRegion frame = (poulet.getState() == EntityState.WALKING)
                     ? pouletWalkAnim.getKeyFrame(poulet.getStateTime(), true)
                     : pouletIdle;
 
             batch.draw(frame,
-                poulet.getPosition().x, poulet.getPosition().y,
-                poulet.getBounds().width / 2, poulet.getBounds().height / 2,
-                poulet.getBounds().width, poulet.getBounds().height,
-                1, 1, poulet.getRotation());
+                    poulet.getPosition().x, poulet.getPosition().y,
+                    poulet.getBounds().width / 2, poulet.getBounds().height / 2,
+                    poulet.getBounds().width, poulet.getBounds().height,
+                    1, 1, poulet.getRotation());
         }
 
         for (Mouton mouton : world.getMoutons()) {
-            TextureRegion frame =
-                (mouton.getState() == EntityState.WALKING)
+            TextureRegion frame = (mouton.getState() == EntityState.WALKING)
                     ? moutonWalkAnim.getKeyFrame(mouton.getStateTime(), true)
                     : moutonIdle;
 
             batch.draw(frame,
-                mouton.getPosition().x, mouton.getPosition().y,
-                mouton.getBounds().width / 2, mouton.getBounds().height / 2,
-                mouton.getBounds().width, mouton.getBounds().height,
-                1, 1, mouton.getRotation());
+                    mouton.getPosition().x, mouton.getPosition().y,
+                    mouton.getBounds().width / 2, mouton.getBounds().height / 2,
+                    mouton.getBounds().width, mouton.getBounds().height,
+                    1, 1, mouton.getRotation());
         }
 
         if (world.getTarget() != null) {
             batch.draw(caveTex, world.getTarget().x, world.getTarget().y, world.getTarget().width,
-                world.getTarget().height);
+                    world.getTarget().height);
         }
 
         if (world.getExitZone() != null) {
