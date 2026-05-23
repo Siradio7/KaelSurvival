d75d0b04979cc7228b85de492668075a7a96ada5
Siradio7 <mamadousiradioudiallo4@gmail.com>
2026-05-23 02:15:23 +0200

    fix scene size

--- core/src/main/java/com/java_game_project/screens/GameScreen.java
+++ core/src/main/java/com/java_game_project/screens/GameScreen.java
@@ -108,7 +108,7 @@ public class GameScreen extends AbstractScreen {
         try {
             controller.update(delta, camera);
 
-            Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            Gdx.gl.glViewport(0, 0, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight());
             Gdx.gl.glClearColor(0, 0, 0, 1);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
